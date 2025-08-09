31cc67aa9744d7a0bc39b3104a9b277b180cdc07
abdulbasitsuleiman <ask4bst@gmail.com>
2025-08-09 06:44:08 +0100

    Create __manifest__.py

---
+++ supreme_api/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Supreme HRMS API',
+    'version': '18.0.1.0.0',
+    'category': 'Human Resources',
+    'summary': 'REST API for Supreme HRMS',
+    'author': 'Supreme Stack Systems Limited',
+    'website': 'https://www.supremestack.net',
+    'license': 'LGPL-3',
+    'depends': [
+        'supreme_core',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+}
