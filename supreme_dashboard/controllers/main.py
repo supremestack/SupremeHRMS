555d0c3a14aeed884ac8fcbe8d639c45e6be6ef6
abdulbasitsuleiman <ask4bst@gmail.com>
2025-08-09 06:12:33 +0100

    Update main.py

--- supreme_dashboard/controllers/main.py
+++ supreme_dashboard/controllers/main.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
 
+class SupremeDashboardController(http.Controller):
+    """Dashboard controller"""
+    
+    @http.route('/supreme/dashboard/data', type='json', auth='user')
+    def get_dashboard_data(self, **kwargs):
+        """Get dashboard metrics"""
+        return request.env['hr.dashboard'].sudo().get_dashboard_data()
